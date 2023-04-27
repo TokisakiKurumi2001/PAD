54e6fc6f62e1d80c506169f92565724309a237ca
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2023-04-27 16:41:28 +0000

    Remove unneccessary in pl_wrapper.py

--- PAD/pl_wrapper.py
+++ PAD/pl_wrapper.py
@@ -30,25 +30,22 @@ class LitPAD(pl.LightningModule):
         m, c = self.model(batch)
 
         mlm_loss = self.mlm_loss(m.view(-1, self.mlm_vocab_size), mlm_labels.view(-1).long())
+        mlm_loss = torch.nan_to_num(mlm_loss)
         cls_loss = self.cls_loss(c.view(-1, self.cls_class_num), cls_labels.view(-1).long())
 
-        loss = torch.nan_to_num(mlm_loss) + cls_loss
+        loss = mlm_loss + cls_loss
         self.log("train/mlm_loss", mlm_loss, sync_dist=True)
         self.log("train/cls_loss", cls_loss, sync_dist=True)
         self.log("train/loss", loss, sync_dist=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        mlm_labels = batch.pop('decoder_labels')
+        batch.pop('decoder_labels')
         cls_labels = batch.pop('cls_label')
-        m, c = self.model(batch)
+        _, c = self.model(batch)
 
-        mlm_loss = self.mlm_loss(m.view(-1, self.mlm_vocab_size), mlm_labels.view(-1).long())
-        cls_loss = self.cls_loss(c.view(-1, self.cls_class_num), cls_labels.view(-1).long())
+        loss = self.cls_loss(c.view(-1, self.cls_class_num), cls_labels.view(-1).long())
 
-        loss = torch.nan_to_num(mlm_loss) + cls_loss
-        self.log("valid/mlm_loss", mlm_loss, sync_dist=True)
-        self.log("valid/cls_loss", cls_loss, sync_dist=True)
         self.log("valid/loss", loss, sync_dist=True)
 
         cls_preds = c.argmax(dim=-1)
@@ -59,23 +56,19 @@ class LitPAD(pl.LightningModule):
         self.log('valid/accuracy', results['accuracy'], on_epoch=True, on_step=False, sync_dist=True)
 
     def test_step(self, batch, batch_idx):
-        mlm_labels = batch.pop('decoder_labels')
+        batch.pop('decoder_labels')
         cls_labels = batch.pop('cls_label')
-        m, c = self.model(batch)
+        _, c = self.model(batch)
 
-        mlm_loss = self.mlm_loss(m.view(-1, self.mlm_vocab_size), mlm_labels.view(-1).long())
-        cls_loss = self.cls_loss(c.view(-1, self.cls_class_num), cls_labels.view(-1).long())
+        loss = self.cls_loss(c.view(-1, self.cls_class_num), cls_labels.view(-1).long())
 
-        loss = torch.nan_to_num(mlm_loss) + cls_loss
-        self.log("test/mlm_loss", mlm_loss, sync_dist=True)
-        self.log("test/cls_loss", cls_loss, sync_dist=True)
         self.log("test/loss", loss, sync_dist=True)
 
         cls_preds = c.argmax(dim=-1)
         self.cls_test_metric.add_batch(predictions=cls_preds, references=cls_labels)
 
     def test_epoch_end(self, outputs):
-        results = self.cls_valid_metric.compute()
+        results = self.cls_test_metric.compute()
         self.log('test/accuracy', results['accuracy'], on_epoch=True, on_step=False, sync_dist=True)
         
     def configure_optimizers(self):
